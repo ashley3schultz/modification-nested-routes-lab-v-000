20985fd51e66d8ff8d781a57635ea3b973c39f07
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-10 00:26:20 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -26,7 +26,7 @@ class SongsController < ApplicationController
 
   def new
     if params[:artist_id]
-      @artist = Artist.find_by(params[:artist_id])
+      @artist = Artist.find_by[id: params[:artist_id])
       redirect_to artists_path, alert: "Artist not found."
     else 
       @song = Song.new(artist_id: params[:artist_id])
